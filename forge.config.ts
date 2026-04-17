397c0b00d185d6677a173b149fe3d7a241e8af10
Erick Zhao <ezhao@slack-corp.com>
2026-04-17 15:37:40 -0700

    tag push publish

--- forge.config.ts
+++ forge.config.ts
@@ -6,6 +6,7 @@ import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import { PublisherGithub } from '@electron-forge/publisher-github';
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -17,6 +18,16 @@ const config: ForgeConfig = {
     new MakerZIP({}, ['darwin']),
     new MakerRpm({}),
     new MakerDeb({}),
+  ],
+  publishers: [
+    new PublisherGithub({
+      repository: {
+        owner: process.env.GITHUB_REPOSITORY_OWNER || '',
+        name: process.env.GITHUB_REPOSITORY_NAME || '',
+      },
+      prerelease: false,
+      draft: false,
+    }),
   ],
   plugins: [
     new VitePlugin({
